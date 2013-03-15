5f32db1bb0567edcd6b10b6f21735162c57634fe
ourocredcode@gmail.com <ourocredcode@gmail.com@80be387f-e333-0be6-272b-7783be4b9d33>
2013-03-15 20:49:29 +0000

    [Felipe Almeida]

--- trunk/sgo/src/br/com/sgo/controller/ParceiroNegocioController.java
+++ trunk/sgo/src/br/com/sgo/controller/ParceiroNegocioController.java
@@ -5,30 +5,29 @@ import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
-import br.com.caelum.vraptor.Validator;
 import br.com.caelum.vraptor.view.Results;
 import br.com.sgo.dao.BancoDao;
 import br.com.sgo.dao.CategoriaParceiroDao;
 import br.com.sgo.dao.ClassificacaoParceiroDao;
-import br.com.sgo.dao.ColunaBdDao;
-import br.com.sgo.dao.ElementoBdDao;
+import br.com.sgo.dao.DepartamentoDao;
 import br.com.sgo.dao.EmpresaDao;
+import br.com.sgo.dao.FuncaoDao;
+import br.com.sgo.dao.FuncionarioDao;
 import br.com.sgo.dao.GrupoParceiroDao;
 import br.com.sgo.dao.IdiomaDao;
 import br.com.sgo.dao.OrganizacaoDao;
 import br.com.sgo.dao.ParceiroNegocioDao;
-import br.com.sgo.dao.TabelaBdDao;
-import br.com.sgo.dao.TipoDadoBdDao;
 import br.com.sgo.dao.TipoParceiroDao;
 import br.com.sgo.interceptor.Public;
-import br.com.sgo.modelo.ColunaBd;
+import br.com.sgo.interceptor.UsuarioInfo;
+import br.com.sgo.modelo.Funcionario;
 import br.com.sgo.modelo.ParceiroNegocio;
 
 @Resource
-public class ParceiroNegocioController {
+public class ParceironegocioController {
 
 	private final Result result;
-	private final Validator validator;
+	private final UsuarioInfo usuarioInfo;
 	private final ParceiroNegocioDao parceiroNegocioDao;
 	private final EmpresaDao empresaDao;
 	private final OrganizacaoDao organizacaoDao;
@@ -36,15 +35,18 @@ public class ParceiroNegocioController {
 	private final CategoriaParceiroDao categoriaParceiroDao;	
 	private final ClassificacaoParceiroDao classificacaoParceiroDao;		
 	private final TipoParceiroDao tipoParceiroDao;
+	private final FuncionarioDao funcionarioDao;
 	private final BancoDao bancoDao;
 	private final IdiomaDao idiomaDao;
+	private final DepartamentoDao departamentoDao;
+	private final FuncaoDao funcaoDao;
+	
 
-	public ParceiroNegocioController(Result result, Validator validator, ParceiroNegocioDao parceiroNegocioDao, EmpresaDao empresaDao,OrganizacaoDao organizacaoDao,GrupoParceiroDao grupoParceiroDao
+	public ParceironegocioController(Result result, UsuarioInfo usuarioInfo,ParceiroNegocioDao parceiroNegocioDao, EmpresaDao empresaDao,OrganizacaoDao organizacaoDao,GrupoParceiroDao grupoParceiroDao
 			,CategoriaParceiroDao categoriaParceiroDao,ClassificacaoParceiroDao classificacaoParceiroDao,TipoParceiroDao tipoParceiroDao,
-			BancoDao bancoDao,IdiomaDao idiomaDao) {
+			BancoDao bancoDao,IdiomaDao idiomaDao,DepartamentoDao departamentoDao,FuncaoDao funcaoDao,FuncionarioDao funcionarioDao) {
 
 		this.result = result;
-		this.validator=validator;
 		this.parceiroNegocioDao = parceiroNegocioDao;
 		this.empresaDao = empresaDao;
 		this.organizacaoDao = organizacaoDao;
@@ -54,13 +56,21 @@ public class ParceiroNegocioController {
 		this.tipoParceiroDao = tipoParceiroDao;
 		this.bancoDao = bancoDao;
 		this.idiomaDao = idiomaDao;
+		this.departamentoDao = departamentoDao;
+		this.funcaoDao = funcaoDao;
+		this.usuarioInfo = usuarioInfo;
+		this.funcionarioDao = funcionarioDao;
 
 	}
 
 	@Get
-	@Public
 	@Path("/parceironegocio/cadastro")
 	public void cadastro(){
+
+		result.include("departamentos", this.departamentoDao.buscaDepartamentos(usuarioInfo.getUsuario().getEmpresa().getEmpresa_id(), 
+					usuarioInfo.getUsuario().getOrganizacao().getOrganizacao_id()));
+		result.include("funcoes", this.funcaoDao.buscaFuncoes(usuarioInfo.getUsuario().getEmpresa().getEmpresa_id(), 
+				usuarioInfo.getUsuario().getOrganizacao().getOrganizacao_id()));
 
 	}
 
@@ -68,35 +78,34 @@ public class ParceiroNegocioController {
 	@Post
 	@Public
 	@Path("/parceironegocio/salva")
-	public void salva(ParceiroNegocio parceiroNegocio){
-
-		validator.validate(parceiroNegocio);
-		validator.onErrorUsePageOf(this).cadastro();
+	public void salva(ParceiroNegocio parceiroNegocio,Funcionario funcionario){
 
 		String mensagem = "";
-
-		try {
-
-			parceiroNegocio.setEmpresa(this.empresaDao.load(parceiroNegocio.getEmpresa().getEmpresa_id()));		
-			parceiroNegocio.setOrganizacao(this.organizacaoDao.load(parceiroNegocio.getOrganizacao().getOrganizacao_id()));			
 
 			this.parceiroNegocioDao.beginTransaction();
 			this.parceiroNegocioDao.adiciona(parceiroNegocio);
 			this.parceiroNegocioDao.commit();
 
-			mensagem = "Parceiro de Negócios " + parceiroNegocio.getNome() + " adicionado com sucesso";			
-			
-		} catch(Exception e) {
-			
-			this.parceiroNegocioDao.rollback();
+			if(parceiroNegocio.getIsFuncionario()){
 
-			if (e.getCause().toString().indexOf("IX_COLUNABD_ELEMENTOBDID") != -1){
-				mensagem = "Erro: Parceiro de negócios " + parceiroNegocio.getNome() + " já existente.";
-			} else {
-				mensagem = "Erro ao adicionar o Parceiro de negócios.";
+				Funcionario f = new Funcionario();
+
+				f.setEmpresa(parceiroNegocio.getEmpresa());
+				f.setOrganizacao(parceiroNegocio.getOrganizacao());
+				f.setDepartamento(funcionario.getDepartamento());
+				f.setFuncao(funcionario.getFuncao());
+				f.setApelido(funcionario.getApelido());
+				f.setIsActive(parceiroNegocio.getIsActive());
+
+				this.funcionarioDao.beginTransaction();
+				this.funcionarioDao.adiciona(f);
+				this.funcionarioDao.commit();
+
 			}
 
-		}
+			mensagem = "Parceiro de Negócios " + parceiroNegocio.getNome() + " adicionado com sucesso";			
+			
+		
 
 		this.parceiroNegocioDao.clear();
 		this.parceiroNegocioDao.close();
