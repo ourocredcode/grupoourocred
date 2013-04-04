73a63454684ddbee8966705e101399ac588b03ac
ourocredcode <ourocredcode@gmail.com>
2013-04-04 15:11:48 +0000

    [André Alexandre]

--- sgo/src/br/com/sgo/dao/ParceiroContatoDao.java
+++ sgo/src/br/com/sgo/dao/ParceiroContatoDao.java
@@ -23,12 +23,14 @@ public class ParceiroContatoDao extends Dao<ParceiroContato> {
 	private Connection conn;
 	private ResultSet rsParceiroContato;
 	
-	private String sqlParceiroContato = " SELECT PARCEIROCONTATO.parceirocontato_id, PARCEIROCONTATO.tipocontato_id, TIPOCONTATO.tipocontato_id, " +
-			"		TIPOCONTATO.nome as tipocontato_nome , TIPOCONTATO.chave as tipocontato_chave , PARCEIROCONTATO.nome as parceirocontato_nome " +
-			"			FROM " +
-			"				(PARCEIROCONTATO (NOLOCK) INNER JOIN PARCEIRONEGOCIO (NOLOCK) ON PARCEIROCONTATO.parceironegocio_id = PARCEIRONEGOCIO.parceironegocio_id) " +
-			"				INNER JOIN TIPOCONTATO (NOLOCK) ON PARCEIROCONTATO.tipocontato_id = TIPOCONTATO.tipocontato_id " +
-			"			WHERE PARCEIROCONTATO.isactive = 1 ";
+	private String sqlParceiroContatoAll = "SELECT PARCEIROCONTATO.parceirocontato_id, PARCEIROCONTATO.empresa_id, PARCEIROCONTATO.organizacao_id," +
+											" PARCEIROCONTATO.parceironegocio_id, PARCEIROCONTATO.tipocontato_id, PARCEIROCONTATO.nome	FROM PARCEIROCONTATO (NOLOCK)";
+	
+	private String sqlParceiroContato = "SELECT PARCEIROCONTATO.parceirocontato_id, PARCEIROCONTATO.nome, PARCEIROCONTATO.empresa_id, PARCEIROCONTATO.organizacao_id, " +
+			"PARCEIROCONTATO.parceironegocio_id, PARCEIROCONTATO.tipocontato_id	FROM (((PARCEIROCONTATO (NOLOCK) INNER JOIN EMPRESA (NOLOCK) " +
+			"ON PARCEIROCONTATO.empresa_id = EMPRESA.empresa_id) INNER JOIN ORGANIZACAO (NOLOCK) ON PARCEIROCONTATO.organizacao_id = ORGANIZACAO.organizacao_id) " +
+			"INNER JOIN TIPOCONTATO (NOLOCK) ON PARCEIROCONTATO.tipocontato_id = TIPOCONTATO.tipocontato_id) " +
+			"INNER JOIN PARCEIRONEGOCIO (NOLOCK) ON PARCEIROCONTATO.parceironegocio_id = PARCEIRONEGOCIO.parceironegocio_id";
 
 	public ParceiroContatoDao(Session session,ConnJDBC conexao) {
 		super(session, ParceiroContato.class);
@@ -36,81 +38,85 @@ public class ParceiroContatoDao extends Dao<ParceiroContato> {
 	}
 	
 	public Collection<ParceiroContato> buscaParceiroContatos(Long parceiroNegocio_id){
-
 		if(parceiroNegocio_id != null)
-			sqlParceiroContato += " AND PARCEIRONEGOCIO.parceironegocio_id = ? ";
-
+			sqlParceiroContato += " WHERE PARCEIRONEGOCIO.parceironegocio_id = ? ";
+		
 		this.conn = this.conexao.getConexao();
-
 		Collection<ParceiroContato> parceiroContatos = new ArrayList<ParceiroContato>();
-
+		
 		try {
-
 			this.stmt = conn.prepareStatement(sqlParceiroContato);			
-			this.stmt.setLong(1,parceiroNegocio_id);			
-
+			this.stmt.setLong(1,parceiroNegocio_id);
 			this.rsParceiroContato = this.stmt.executeQuery();
-
 			while (rsParceiroContato.next()) {
-
 				ParceiroContato parceiroContato = new ParceiroContato();
-
 				parceiroContato.setParceiroContato_id(rsParceiroContato.getLong("parceirocontato_id"));
 				parceiroContato.setNome(rsParceiroContato.getString("parceirocontato_nome"));
-
+				
 				TipoContato tipoContato = new TipoContato();
-
 				tipoContato.setTipoContato_id(rsParceiroContato.getLong("tipocontato_id"));
 				tipoContato.setNome(rsParceiroContato.getString("tipocontato_nome"));
-				tipoContato.setChave(rsParceiroContato.getString("tipocontato_chave"));
-
 				parceiroContato.setTipoContato(tipoContato);
-
+				
 				parceiroContatos.add(parceiroContato);
-
 			}
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 		this.conexao.closeConnection(rsParceiroContato, stmt, conn);
 		return parceiroContatos;
-
 	}
 	
-	public ParceiroContato buscaParceiroContato(Long tipoContato_id, String nome){
-
-		String sql = "SELECT PARCEIROCONTATO.parceirocontato_id " +
-				"	FROM PARCEIROCONTATO WHERE PARCEIROCONTATO.tipocontato_id = ? AND nome like ? AND isactive = 1 ";
-
+	public ParceiroContato buscaParceiroContatoByEmOrPnegTcNo(Long empresa_id, Long organizacao_id, Long parceironegocio_id, Long tipocontato_id, String nome){		
+		if(empresa_id != null)
+			sqlParceiroContatoAll +=" WHERE PARCEIROCONTATO.empresa_id = ? ";
+		if(organizacao_id != null)
+			sqlParceiroContatoAll += 	" AND PARCEIROCONTATO.organizacao_id = ?";
+		if(parceironegocio_id != null)
+			sqlParceiroContatoAll += 	" AND PARCEIROCONTATO.parceironegocio_id = ?";
+		if(tipocontato_id != null)
+			sqlParceiroContatoAll += 	" AND PARCEIROCONTATO.tipocontato_id = ?";
+		if(nome != null)
+			sqlParceiroContatoAll +=	" AND PARCEIROCONTATO.nome like ? ";
+		
 		this.conn = this.conexao.getConexao();
-
-		ParceiroContato parceiroContato = null;
-
+		ParceiroContato parceiroCont = new ParceiroContato();
 		try {
-
-			this.stmt = conn.prepareStatement(sql);			
-			this.stmt.setLong(1,tipoContato_id);
-			this.stmt.setString(2,'%' + nome + '%');
-
+			this.stmt = conn.prepareStatement(sqlParceiroContatoAll);			
+			this.stmt.setLong(1, empresa_id);		
+			this.stmt.setLong(2, organizacao_id);
+			this.stmt.setLong(3, parceironegocio_id);
+			this.stmt.setLong(4, tipocontato_id);
+			this.stmt.setString(5,"%"+  nome + "%");			
 			this.rsParceiroContato = this.stmt.executeQuery();
-
 			while (rsParceiroContato.next()) {
-				
-				parceiroContato = new ParceiroContato();
-
-				parceiroContato.setParceiroContato_id(rsParceiroContato.getLong("parceirocontato_id"));
-
+				parceiroCont.setParceiroContato_id(rsParceiroContato.getLong("parceirocontato_id"));
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		this.conexao.closeConnection(rsParceiroContato, stmt, conn);
+		return parceiroCont;
+	}
 
+	public ParceiroContato buscaParceiroContatoAll(){				
+		this.conn = this.conexao.getConexao();
+		ParceiroContato parceiroContato = null;
+		
+		try {
+			this.stmt = conn.prepareStatement(sqlParceiroContatoAll);			
+			this.rsParceiroContato = this.stmt.executeQuery();
+			while (rsParceiroContato.next()) {				
+				parceiroContato = new ParceiroContato();
+				parceiroContato.setParceiroContato_id(rsParceiroContato.getLong("parceirocontato_id"));				
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
+		
 		this.conexao.closeConnection(rsParceiroContato, stmt, conn);
 		return parceiroContato;
-
 	}
-
+	
 }
