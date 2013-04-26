bd6f279a86b245a6c53f8600af5f74ec48b12d64
ourocredcode <ourocredcode@gmail.com>
2013-04-26 20:59:20 +0000

    [André Alexandre]

--- sgo/src/br/com/sgo/controller/WorkflowtransicaoController.java
+++ sgo/src/br/com/sgo/controller/WorkflowtransicaoController.java
@@ -24,23 +24,25 @@ public class WorkflowtransicaoController {
 	private final OrganizacaoDao organizacaoDao;
 	private final WorkflowEtapaDao workflowEtapaDao;
 
-	public WorkflowtransicaoController(Result result,  UsuarioInfo usuarioInfo, WorkflowTransicaoDao workflowTransicaoDao, EmpresaDao empresaDao, OrganizacaoDao organizacaoDao
-			, WorkflowEtapaDao workflowEtapaDao) {
+	public WorkflowtransicaoController(Result result,  UsuarioInfo usuarioInfo, WorkflowTransicaoDao workflowTransicaoDao, EmpresaDao empresaDao, OrganizacaoDao organizacaoDao, WorkflowEtapaDao workflowEtapaDao) {
+
 		this.result = result;
 		this.usuarioInfo = usuarioInfo;
 		this.workflowTransicaoDao = workflowTransicaoDao;
 		this.empresaDao = empresaDao;
 		this.organizacaoDao = organizacaoDao;
 		this.workflowEtapaDao = workflowEtapaDao;
-		
+
 	}
 
 	@Get
 	@Public
 	@Path("/workflowtransicao/cadastro")
 	public void cadastro() {
+
 		result.include("workflowEtapas", this.workflowEtapaDao.buscaTodosWorkflowEtapa());
-		result.include("workflowTransicoes", this.workflowTransicaoDao.buscaTodosWorkflowTransicao());		
+		result.include("workflowTransicoes", this.workflowTransicaoDao.buscaTodosWorkflowTransicao());
+
 	}
 
 	@Post
@@ -51,29 +53,33 @@ public class WorkflowtransicaoController {
 		String mensagem = "";
 
 		try {
-	
+
 			if (this.workflowTransicaoDao.buscaWorkflowTransicaoPorEmpresaOrganizacaoWorkflowEtapaProximo(workflowTransicao.getEmpresa().getEmpresa_id(),workflowTransicao.getOrganizacao().getOrganizacao_id(),
-					workflowTransicao.getWorkflowEtapa().getWorkflowEtapa_id(), workflowTransicao.getWorkflowEtapaProximo().getWorkflowEtapa_id()) == null) {				
+					workflowTransicao.getWorkflowEtapa().getWorkflowEtapa_id(), workflowTransicao.getWorkflowEtapaProximo().getWorkflowEtapa_id(), workflowTransicao.getPerfil().getPerfil_id()) == null) {				
 				
 				this.workflowTransicaoDao.beginTransaction();
 				this.workflowTransicaoDao.adiciona(workflowTransicao);
 				this.workflowTransicaoDao.commit();
-				
+
 				mensagem = "Etapa transição adicionado com sucesso.";				
-				
+
 			} else {
+
 				mensagem = "Workflow transição já cadastrado.";
+
 			}
-	
+
 		} catch (Exception e) {
 
 				mensagem = "Erro ao adicionar Workflow :";
 
 		} finally{
+
 			this.workflowTransicaoDao.clear();
 			this.workflowTransicaoDao.close();
+
 		}
-		
+
 		result.include("notice", mensagem);			
 		result.redirectTo(this).cadastro();
 
@@ -82,15 +88,19 @@ public class WorkflowtransicaoController {
 	@Get
 	@Path("/workflowtransicao/busca.json")
 	@Public
-	public void workflowTransicao(Long empresa_id, Long organizacao_id, Long workflowetapa_id, Long workflowetapaproximo_id) {	
-		result.use(Results.json()).withoutRoot().from(workflowTransicaoDao.buscaWorkflowTransicaoPorEmpresaOrganizacaoWorkflowEtapaProximo(empresa_id, organizacao_id, workflowetapa_id, workflowetapaproximo_id)).serialize();
+	public void workflowTransicao(Long empresa_id, Long organizacao_id, Long workflowetapa_id, Long workflowetapaproximo_id, Long perfil_id) {	
+
+		result.use(Results.json()).withoutRoot().from(workflowTransicaoDao.buscaWorkflowTransicaoPorEmpresaOrganizacaoWorkflowEtapaProximo(empresa_id, organizacao_id, workflowetapa_id, workflowetapaproximo_id, perfil_id)).serialize();
+
 	}
 
 	@Post
 	@Path("/workflowtransicao/lista")
 	@Public
 	public void lista(Long empresa_id, Long organizacao_id, String nome) {
+
 		result.include("workflowsEtapa", this.workflowEtapaDao.buscaWorkflowsPorEmpresaOrganizacaoNome(empresa_id, organizacao_id, nome));
+
 	}
 
 	@Get
