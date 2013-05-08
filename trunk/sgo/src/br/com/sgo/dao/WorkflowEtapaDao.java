9f5111ff8b7bce2ac56c0a414d715a6351f11385
ourocredcode@gmail.com <ourocredcode@gmail.com@80be387f-e333-0be6-272b-7783be4b9d33>
2013-05-08 20:43:08 +0000

    [André Alexandre]

--- trunk/sgo/src/br/com/sgo/dao/WorkflowEtapaDao.java
+++ trunk/sgo/src/br/com/sgo/dao/WorkflowEtapaDao.java
@@ -250,9 +250,10 @@ public class WorkflowEtapaDao extends Dao<WorkflowEtapa> {
 
 		String sql = "SELECT " +
 				"		WORKFLOWTRANSICAO.workflowetapaproximo_id, WORKFLOWETAPA.nome FROM " +
-				"	(CONTRATO INNER JOIN (WORKFLOWETAPA INNER JOIN WORKFLOWTRANSICAO ON WORKFLOWETAPA.workflowetapa_id = WORKFLOWTRANSICAO.workflowetapaproximo_id) " +
+				"	(CONTRATO (NOLOCK) INNER JOIN (WORKFLOWETAPA (NOLOCK) INNER JOIN WORKFLOWTRANSICAO (NOLOCK) " +
+				" ON WORKFLOWETAPA.workflowetapa_id = WORKFLOWTRANSICAO.workflowetapaproximo_id) " +
 				"		ON CONTRATO.workflowetapa_id = WORKFLOWTRANSICAO.workflowetapa_id) " +
-				"	 INNER JOIN PERFIL ON WORKFLOWTRANSICAO.perfil_id = PERFIL.perfil_id WHERE CONTRATO.contrato_id = ? AND PERFIL.perfil_id = ? ";
+				"	 INNER JOIN PERFIL (NOLOCK) ON WORKFLOWTRANSICAO.perfil_id = PERFIL.perfil_id WHERE CONTRATO.contrato_id = ? AND PERFIL.perfil_id = ? ";
 
 		this.conn = this.conexao.getConexao();
 
@@ -264,6 +265,97 @@ public class WorkflowEtapaDao extends Dao<WorkflowEtapa> {
 
 			this.stmt.setLong(1, contrato_id);
 			this.stmt.setLong(2, perfil_id);
+
+			this.rsWorkflowEtapa = this.stmt.executeQuery();
+
+			while (rsWorkflowEtapa.next()) {
+
+				WorkflowEtapa workflowEtapa = new WorkflowEtapa();
+
+				workflowEtapa.setWorkflowEtapa_id(rsWorkflowEtapa.getLong("workflowetapaproximo_id"));
+				workflowEtapa.setNome(rsWorkflowEtapa.getString("nome"));
+
+				workflowsEtapa.add(workflowEtapa);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		this.conexao.closeConnection(rsWorkflowEtapa, stmt, conn);
+
+		return workflowsEtapa;
+
+	}
+	
+	public Collection<WorkflowEtapa> buscaWorKFlowEtapaByHisconPerfil(Long empresa_id, Long organizacao_id, Long perfil_id, Long hisconbeneficio_id) {
+
+		String sql = "SELECT HISCONBENEFICIO.hisconbeneficio_id, HISCONBENEFICIO.empresa_id, HISCONBENEFICIO.organizacao_id, WORKFLOWETAPA.nome " +
+				" FROM WORKFLOWETAPA (NOLOCK) INNER JOIN (((HISCONBENEFICIO (NOLOCK) " +
+				" INNER JOIN EMPRESA (NOLOCK) ON HISCONBENEFICIO.empresa_id = EMPRESA.empresa_id) " +
+				" INNER JOIN ORGANIZACAO (NOLOCK) ON HISCONBENEFICIO.organizacao_id = ORGANIZACAO.organizacao_id) " +
+				" INNER JOIN (PERFIL (NOLOCK) INNER JOIN WORKFLOWTRANSICAO (NOLOCK) ON PERFIL.perfil_id = WORKFLOWTRANSICAO.perfil_id) " +
+				" ON HISCONBENEFICIO.workflowetapa_id = WORKFLOWTRANSICAO.workflowetapa_id) " +
+				" ON WORKFLOWETAPA.workflowetapa_id = WORKFLOWTRANSICAO.workflowetapaproximo_id " +
+				" WHERE HISCONBENEFICIO.hisconbeneficio_id = ? AND HISCONBENEFICIO.empresa_id = ? AND HISCONBENEFICIO.organizacao_id = ? AND PERFIL.perfil_id = ? ";
+
+		this.conn = this.conexao.getConexao();
+
+		Collection<WorkflowEtapa> workflowsEtapa = new ArrayList<WorkflowEtapa>();
+
+		try {
+
+			this.stmt = conn.prepareStatement(sql);
+
+			this.stmt.setLong(1, hisconbeneficio_id);
+			this.stmt.setLong(2, empresa_id);
+			this.stmt.setLong(3, organizacao_id);
+			this.stmt.setLong(4, perfil_id);
+
+			this.rsWorkflowEtapa = this.stmt.executeQuery();
+
+			while (rsWorkflowEtapa.next()) {
+
+				WorkflowEtapa workflowEtapa = new WorkflowEtapa();
+
+				workflowEtapa.setWorkflowEtapa_id(rsWorkflowEtapa.getLong("workflowetapaproximo_id"));
+				workflowEtapa.setNome(rsWorkflowEtapa.getString("nome"));
+
+				workflowsEtapa.add(workflowEtapa);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		this.conexao.closeConnection(rsWorkflowEtapa, stmt, conn);
+
+		return workflowsEtapa;
+
+	}
+	
+	public Collection<WorkflowEtapa> buscaWorKFlowEtapaByPerfil(Long empresa_id, Long organizacao_id, Long perfil_id) {
+
+		String sql = "SELECT HISCONBENEFICIO.hisconbeneficio_id, HISCONBENEFICIO.empresa_id, HISCONBENEFICIO.organizacao_id, WORKFLOWETAPA.nome " +
+				" FROM WORKFLOWETAPA (NOLOCK) INNER JOIN (((HISCONBENEFICIO (NOLOCK) " +
+				" INNER JOIN EMPRESA (NOLOCK) ON HISCONBENEFICIO.empresa_id = EMPRESA.empresa_id) " +
+				" INNER JOIN ORGANIZACAO (NOLOCK) ON HISCONBENEFICIO.organizacao_id = ORGANIZACAO.organizacao_id) " +
+				" INNER JOIN (PERFIL (NOLOCK) INNER JOIN WORKFLOWTRANSICAO (NOLOCK) ON PERFIL.perfil_id = WORKFLOWTRANSICAO.perfil_id) " +
+				" ON HISCONBENEFICIO.workflowetapa_id = WORKFLOWTRANSICAO.workflowetapa_id) " +
+				" ON WORKFLOWETAPA.workflowetapa_id = WORKFLOWTRANSICAO.workflowetapaproximo_id " +
+				" WHERE HISCONBENEFICIO.empresa_id = ? AND HISCONBENEFICIO.organizacao_id = ? AND PERFIL.perfil_id = ? ";
+
+		this.conn = this.conexao.getConexao();
+
+		Collection<WorkflowEtapa> workflowsEtapa = new ArrayList<WorkflowEtapa>();
+
+		try {
+
+			this.stmt = conn.prepareStatement(sql);
+
+			this.stmt.setLong(1, empresa_id);
+			this.stmt.setLong(2, organizacao_id);
+			this.stmt.setLong(3, perfil_id);
 
 			this.rsWorkflowEtapa = this.stmt.executeQuery();
 
