3d00c9b33f027bfde5c91228d9ccae5dc7ef16f3
ourocredcode <ourocredcode@gmail.com>
2013-05-10 20:07:37 +0000

    [André Alexandre]

---
+++ sgo/src/br/com/sgo/modelo/Periodo.java
@@ -0,0 +1,94 @@
+package br.com.sgo.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import br.com.caelum.vraptor.ioc.Component;
+
+@Entity
+@Component
+@Table(name = "PERIODO")
+public class Periodo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "periodo_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long periodo_id;
+
+	@ManyToOne
+	@JoinColumn(name = "empresa_id", updatable = true, nullable = false)
+	private Empresa empresa;
+
+	@ManyToOne
+	@JoinColumn(name = "organizacao_id", updatable = true, nullable = false)
+	private Organizacao organizacao;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "descricao")
+	private String descricao;
+
+	@Column(name = "isactive")
+	private Boolean isActive;
+
+	
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	public Organizacao getOrganizacao() {
+		return organizacao;
+	}
+
+	public void setOrganizacao(Organizacao organizacao) {
+		this.organizacao = organizacao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public Long getPeriodo_id() {
+		return periodo_id;
+	}
+
+	public void setPeriodo_id(Long periodo_id) {
+		this.periodo_id = periodo_id;
+	}
+
+}
