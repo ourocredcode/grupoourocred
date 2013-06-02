d508f9233def399c8ea7ff28b5166a7e4b0c02a9
ourocredcode@gmail.com <ourocredcode@gmail.com@80be387f-e333-0be6-272b-7783be4b9d33>
2013-06-02 14:27:28 +0000

    [André Alexandre]

--- trunk/sgo/src/br/com/sgo/dao/PerfilDao.java
+++ trunk/sgo/src/br/com/sgo/dao/PerfilDao.java
@@ -21,16 +21,107 @@ public class PerfilDao extends Dao<Perfil> {
 	private PreparedStatement stmt;
 	private Connection conn;
 	private ResultSet rsPerfil;
+	
+	private final String sqlPerfil = "select PERFIL.empresa_id, PERFIL.organizacao_id, PERFIL.perfil_id, PERFIL.nome FROM PERFIL (NOLOCK) ";
 
 	public PerfilDao(Session session, ConnJDBC conexao) {
 		super(session, Perfil.class);
 		this.conexao = conexao;
 	}
 
+	public Collection<Perfil> buscaAllPerfis(Long empresa_id, Long organizacao_id) {
+
+		String sql = sqlPerfil;
+		
+		if (empresa_id != null)
+			sql += " WHERE PERFIL.empresa_id = ? ";		
+		if (organizacao_id != null)
+			sql += " AND PERFIL.organizacao_id = ?";
+
+		this.conn = this.conexao.getConexao();
+
+		Collection<Perfil> perfis = new ArrayList<Perfil>();
+
+		try {
+
+			this.stmt = conn.prepareStatement(sql);
+
+			this.stmt.setLong(1, empresa_id);
+			this.stmt.setLong(2, organizacao_id);
+
+			this.rsPerfil = this.stmt.executeQuery();
+
+			while (rsPerfil.next()) {
+
+				Perfil perfil = new Perfil();
+				perfil.setPerfil_id(rsPerfil.getLong("perfil_id"));
+				perfil.setNome(rsPerfil.getString("nome"));
+				perfis.add(perfil);
+
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
+		this.conexao.closeConnection(rsPerfil, stmt, conn);
+
+		return perfis;
+
+	}
+
+	public Collection<Perfil> buscaPerfisToWorkflowEtapaPerfil() {
+
+		String sql = "SELECT DISTINCT(WORKFLOWPERFILACESSO.perfil_id), PERFIL.nome AS perfil_nome "+
+					" FROM ((ORGANIZACAO (NOLOCK) INNER JOIN (EMPRESA (NOLOCK) INNER JOIN WORKFLOWPERFILACESSO (NOLOCK) "+
+					" ON EMPRESA.empresa_id = WORKFLOWPERFILACESSO.empresa_id) ON ORGANIZACAO.organizacao_id = WORKFLOWPERFILACESSO.organizacao_id) "+ 
+					" INNER JOIN PERFIL (NOLOCK) ON WORKFLOWPERFILACESSO.perfil_id = PERFIL.perfil_id) "+
+					" INNER JOIN WORKFLOW (NOLOCK) ON WORKFLOWPERFILACESSO.workflow_id = WORKFLOW.workflow_id ";
+
+		this.conn = this.conexao.getConexao();
+
+		Collection<Perfil> perfis = new ArrayList<Perfil>();
+
+		try {
+
+			this.stmt = conn.prepareStatement(sql);
+
+			this.rsPerfil = this.stmt.executeQuery();
+
+			while (rsPerfil.next()) {
+
+				Perfil perfil = new Perfil();
+				perfil.setPerfil_id(rsPerfil.getLong("perfil_id"));
+				perfil.setNome(rsPerfil.getString("perfil_nome"));
+				perfis.add(perfil);
+
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
+		this.conexao.closeConnection(rsPerfil, stmt, conn);
+
+		return perfis;
+
+	}
+
 	public Collection<Perfil> buscaPerfis(Long empresa_id, Long organizacao_id, String nome) {
 
-		String sql = "select PERFIL.empresa_id, PERFIL.organizacao_id, PERFIL.perfil_id, PERFIL.nome from PERFIL (NOLOCK) "
-				+ "	WHERE PERFIL.empresa_id = ? AND PERFIL.organizacao_id = ? AND PERFIL.nome like ? ";
+		String sql = sqlPerfil;
+		
+		if (empresa_id != null)
+			sql += " WHERE PERFIL.empresa_id = ? ";		
+		if (organizacao_id != null)
+			sql += " AND PERFIL.organizacao_id = ?";
+		if (nome != null)
+			sql += " AND PERFIL.nome = ?";
+
 		this.conn = this.conexao.getConexao();
 
 		Collection<Perfil> perfis = new ArrayList<Perfil>();
