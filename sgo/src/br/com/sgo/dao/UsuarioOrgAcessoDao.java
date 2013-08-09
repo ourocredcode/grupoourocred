abfd9e1a946fa04b1798f95ddac96e71abebd0a0
ourocredcode <ourocredcode@gmail.com>
2013-08-09 14:50:17 +0000

    [André Alexandre]

--- sgo/src/br/com/sgo/dao/UsuarioOrgAcessoDao.java
+++ sgo/src/br/com/sgo/dao/UsuarioOrgAcessoDao.java
@@ -24,24 +24,25 @@ public class UsuarioOrgAcessoDao extends Dao<UsuarioOrgAcesso> {
 	private ConnJDBC conexao;
 	private PreparedStatement stmt;
 	private Connection conn;
-	private ResultSet rsUsuarioOrgAcesso; 
-	
+	private ResultSet rsUsuarioOrgAcesso;
+
 	private final String sqlUsuarioOrgAcesso = "SELECT USUARIOORGACESSO.organizacao_id, USUARIOORGACESSO.empresa_id, USUARIOORGACESSO.usuario_id FROM USUARIOORGACESSO (NOLOCK) ";
 
-	private final String sqlUsuariosOrgAcesso = "SELECT USUARIOORGACESSO.empresa_id, EMPRESA.nome as empresa_nome, USUARIOORGACESSO.organizacao_id "+
-						", ORGANIZACAO.nome AS organizacao_nome, USUARIOORGACESSO.usuario_id, USUARIO.nome AS usuario_nome, USUARIO.chave, USUARIOORGACESSO.isactive "+
-						" FROM ((USUARIOORGACESSO (NOLOCK) INNER JOIN EMPRESA (NOLOCK) ON USUARIOORGACESSO.empresa_id = EMPRESA.empresa_id) "+ 
-						" INNER JOIN ORGANIZACAO(NOLOCK) ON USUARIOORGACESSO.organizacao_id = ORGANIZACAO.organizacao_id) "+
-						" INNER JOIN USUARIO (NOLOCK) ON USUARIOORGACESSO.usuario_id = USUARIO.usuario_id ";
-	
+	private final String sqlUsuariosOrgAcesso = "SELECT USUARIOORGACESSO.empresa_id, EMPRESA.nome as empresa_nome, USUARIOORGACESSO.organizacao_id "
+			+ ", ORGANIZACAO.nome AS organizacao_nome, USUARIOORGACESSO.usuario_id, USUARIO.nome AS usuario_nome, USUARIO.chave, USUARIOORGACESSO.isactive "
+			+ " FROM ((USUARIOORGACESSO (NOLOCK) INNER JOIN EMPRESA (NOLOCK) ON USUARIOORGACESSO.empresa_id = EMPRESA.empresa_id) "
+			+ " INNER JOIN ORGANIZACAO(NOLOCK) ON USUARIOORGACESSO.organizacao_id = ORGANIZACAO.organizacao_id) "
+			+ " INNER JOIN USUARIO (NOLOCK) ON USUARIOORGACESSO.usuario_id = USUARIO.usuario_id ";
+
 	public UsuarioOrgAcessoDao(Session session, ConnJDBC conexao) {
 
 		super(session, UsuarioOrgAcesso.class);
 		this.conexao = conexao;
 
 	}
-	
-	public Collection<UsuarioOrgAcesso> buscaAllUsuarioOrgAcessoByEmpresaOrganizacao(Long empresa_id, Long organizacao_id) {
+
+	public Collection<UsuarioOrgAcesso> buscaAllUsuarioOrgAcessoByEmpresaOrganizacao(
+			Long empresa_id, Long organizacao_id) {
 
 		String sql = sqlUsuariosOrgAcesso;
 
@@ -73,17 +74,20 @@ public class UsuarioOrgAcessoDao extends Dao<UsuarioOrgAcesso> {
 				empresa.setEmpresa_id(rsUsuarioOrgAcesso.getLong("empresa_id"));
 				empresa.setNome(rsUsuarioOrgAcesso.getString("empresa_nome"));
 
-				organizacao.setOrganizacao_id(rsUsuarioOrgAcesso.getLong("organizacao_id"));
-				organizacao.setNome(rsUsuarioOrgAcesso.getString("organizacao_nome"));				
+				organizacao.setOrganizacao_id(rsUsuarioOrgAcesso
+						.getLong("organizacao_id"));
+				organizacao.setNome(rsUsuarioOrgAcesso
+						.getString("organizacao_nome"));
 
 				usuario.setUsuario_id(rsUsuarioOrgAcesso.getLong("usuario_id"));
-				usuario.setNome(rsUsuarioOrgAcesso.getString("usuario_nome"));	
+				usuario.setNome(rsUsuarioOrgAcesso.getString("usuario_nome"));
 				usuario.setChave(rsUsuarioOrgAcesso.getString("chave"));
 
 				usuarioOrgAcesso.setEmpresa(empresa);
 				usuarioOrgAcesso.setOrganizacao(organizacao);
-				usuarioOrgAcesso.setUsuario(usuario);				
-				usuarioOrgAcesso.setIsActive(rsUsuarioOrgAcesso.getBoolean("isactive"));
+				usuarioOrgAcesso.setUsuario(usuario);
+				usuarioOrgAcesso.setIsActive(rsUsuarioOrgAcesso
+						.getBoolean("isactive"));
 
 				usuariosOrgAcesso.add(usuarioOrgAcesso);
 
@@ -99,12 +103,13 @@ public class UsuarioOrgAcessoDao extends Dao<UsuarioOrgAcesso> {
 
 	}
 
-	public UsuarioOrgAcesso buscaUsuarioOrgAcessoByEmpresaOrganizacaoUsuarioPerfil(Long empresa_id, Long organizacao_id, Long usuario_id) {
+	public UsuarioOrgAcesso buscaUsuarioOrgAcessoByEmpresaOrganizacaoUsuarioPerfil(
+			Long empresa_id, Long organizacao_id, Long usuario_id) {
 
-		String sql = sqlUsuarioOrgAcesso; 
+		String sql = sqlUsuarioOrgAcesso;
 
 		this.conn = this.conexao.getConexao();
-		
+
 		if (empresa_id != null)
 			sql += " WHERE USUARIOORGACESSO.empresa_id = ?";
 		if (organizacao_id != null)
@@ -126,12 +131,22 @@ public class UsuarioOrgAcessoDao extends Dao<UsuarioOrgAcesso> {
 
 			while (rsUsuarioOrgAcesso.next()) {
 
-				usuarioOrgAcesso = new UsuarioOrgAcesso();				
+				Empresa empresa = new Empresa();
+				Organizacao organizacao = new Organizacao();
+				usuarioOrgAcesso = new UsuarioOrgAcesso();
 				Usuario usuario = new Usuario();
-				usuario.setUsuario_id(rsUsuarioOrgAcesso.getLong("usuario_id"));				
+
+				empresa.setEmpresa_id(rsUsuarioOrgAcesso.getLong("empresa_id"));
+				organizacao.setOrganizacao_id(rsUsuarioOrgAcesso
+						.getLong("organizacao_id"));
+				usuario.setUsuario_id(rsUsuarioOrgAcesso.getLong("usuario_id"));
+
+				usuarioOrgAcesso.setEmpresa(empresa);
+				usuarioOrgAcesso.setOrganizacao(organizacao);
 				usuarioOrgAcesso.setUsuario(usuario);
 
 			}
+
 		} catch (SQLException e) {
 
 			e.printStackTrace();
@@ -156,11 +171,16 @@ public class UsuarioOrgAcessoDao extends Dao<UsuarioOrgAcesso> {
 
 			this.stmt.setLong(1, usuarioOrgAcesso.getUsuario().getUsuario_id());
 			this.stmt.setLong(2, usuarioOrgAcesso.getEmpresa().getEmpresa_id());
-			this.stmt.setLong(3, usuarioOrgAcesso.getOrganizacao().getOrganizacao_id());		
-			this.stmt.setTimestamp(4, new Timestamp(usuarioOrgAcesso.getCreated().getTimeInMillis()));
-			this.stmt.setTimestamp(5, new Timestamp(usuarioOrgAcesso.getUpdated().getTimeInMillis()));
-			this.stmt.setLong(6, usuarioOrgAcesso.getCreatedBy().getUsuario_id());
-			this.stmt.setLong(7, usuarioOrgAcesso.getUpdatedBy().getUsuario_id());
+			this.stmt.setLong(3, usuarioOrgAcesso.getOrganizacao()
+					.getOrganizacao_id());
+			this.stmt.setTimestamp(4, new Timestamp(usuarioOrgAcesso
+					.getCreated().getTimeInMillis()));
+			this.stmt.setTimestamp(5, new Timestamp(usuarioOrgAcesso
+					.getUpdated().getTimeInMillis()));
+			this.stmt.setLong(6, usuarioOrgAcesso.getCreatedBy()
+					.getUsuario_id());
+			this.stmt.setLong(7, usuarioOrgAcesso.getUpdatedBy()
+					.getUsuario_id());
 			this.stmt.setBoolean(8, usuarioOrgAcesso.getIsActive());
 
 			this.stmt.executeUpdate();
@@ -177,6 +197,47 @@ public class UsuarioOrgAcessoDao extends Dao<UsuarioOrgAcesso> {
 			this.conn.setAutoCommit(true);
 
 		}
+		this.conexao.closeConnection(stmt, conn);
+	}
+
+	public void altera(UsuarioOrgAcesso usuarioOrgAcesso) throws SQLException {
+
+		String sql = "UPDATE UsuarioOrgAcesso SET updated=? , updatedby=?, isactive=? "
+				+ " WHERE UsuarioOrgAcesso.empresa_id=? AND UsuarioOrgAcesso.organizacao_id=? AND UsuarioOrgAcesso.usuario_id=? ";
+
+		this.conn = this.conexao.getConexao();
+
+		try {
+
+			this.conn.setAutoCommit(false);
+
+			this.stmt = conn.prepareStatement(sql);
+
+			this.stmt.setTimestamp(1, new Timestamp(usuarioOrgAcesso
+					.getUpdated().getTimeInMillis()));
+			this.stmt.setLong(2, usuarioOrgAcesso.getUpdatedBy()
+					.getUsuario_id());
+			this.stmt.setBoolean(3, usuarioOrgAcesso.getIsActive());
+			this.stmt.setLong(4, usuarioOrgAcesso.getEmpresa().getEmpresa_id());
+			this.stmt.setLong(5, usuarioOrgAcesso.getOrganizacao()
+					.getOrganizacao_id());
+			this.stmt.setLong(6, usuarioOrgAcesso.getUsuario().getUsuario_id());
+
+			this.stmt.executeUpdate();
+
+			this.conn.commit();
+
+		} catch (SQLException e) {
+
+			this.conn.rollback();
+			throw e;
+
+		} finally {
+
+			this.conn.setAutoCommit(true);
+
+		}
+
 		this.conexao.closeConnection(stmt, conn);
 	}
 
