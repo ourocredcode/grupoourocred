6196da0c75641821b9f24a531ce8921d776c71c0
ourocredcode <ourocredcode@gmail.com>
2013-08-09 14:52:08 +0000

    [André Alexandre]

--- sgo/src/br/com/sgo/controller/UsuarioorgacessoController.java
+++ sgo/src/br/com/sgo/controller/UsuarioorgacessoController.java
@@ -1,5 +1,6 @@
 package br.com.sgo.controller;
 
+import java.sql.SQLException;
 import java.util.Calendar;
 
 import br.com.caelum.vraptor.Get;
@@ -14,6 +15,7 @@ import br.com.sgo.dao.UsuarioOrgAcessoDao;
 import br.com.sgo.interceptor.UsuarioInfo;
 import br.com.sgo.modelo.Empresa;
 import br.com.sgo.modelo.Organizacao;
+import br.com.sgo.modelo.Usuario;
 import br.com.sgo.modelo.UsuarioOrgAcesso;
 
 @Resource
@@ -22,20 +24,22 @@ public class UsuarioorgacessoController {
 	private final Result result;
 	private final UsuarioInfo usuarioInfo;
 	private final UsuarioOrgAcessoDao usuarioOrgAcessoDao;
-	
+
 	private Empresa empresa;
 	private Organizacao organizacao;
+	private Usuario usuario;
 
 	private Calendar dataAtual = Calendar.getInstance();
 
-	public UsuarioorgacessoController(Result result, UsuarioInfo usuarioInfo, Empresa empresa, Organizacao organizacao, EmpresaDao empresaDao
-			,OrganizacaoDao organizacaoDao,UsuarioDao usuarioDao,UsuarioOrgAcessoDao usuarioOrgAcessoDao){
+	public UsuarioorgacessoController(Result result, UsuarioInfo usuarioInfo,Empresa empresa, Organizacao organizacao, EmpresaDao empresaDao,OrganizacaoDao organizacaoDao
+			, Usuario usuario, UsuarioDao usuarioDao, UsuarioOrgAcessoDao usuarioOrgAcessoDao) {
 
+		this.result = result;
 		this.usuarioInfo = usuarioInfo;
+		this.usuario = this.usuarioInfo.getUsuario();
 		this.empresa = usuarioInfo.getEmpresa();
 		this.organizacao = usuarioInfo.getOrganizacao();
 		this.usuarioOrgAcessoDao = usuarioOrgAcessoDao;
-		this.result = result;
 
 	}
 
@@ -43,51 +47,84 @@ public class UsuarioorgacessoController {
 	@Path("/usuarioorgacesso/cadastro")
 	public void cadastro() {
 
-		result.include("usuariosOrgAcesso", this.usuarioOrgAcessoDao.buscaAllUsuarioOrgAcessoByEmpresaOrganizacao(empresa.getEmpresa_id(), organizacao.getOrganizacao_id()));
+		result.include("usuariosOrgAcesso", this.usuarioOrgAcessoDao.buscaAllUsuarioOrgAcessoByEmpresaOrganizacao(empresa.getEmpresa_id(),organizacao.getOrganizacao_id()));
 
 	}
 
 	@Post
 	@Path("/usuarioorgacesso/salva")
-	public void salva(UsuarioOrgAcesso usuarioOrgAcesso){
+	public void salva(UsuarioOrgAcesso usuarioOrgAcesso) {
 
 		String mensagem = "";
 
 		try {
 
-			if (this.usuarioOrgAcessoDao.buscaUsuarioOrgAcessoByEmpresaOrganizacaoUsuarioPerfil(empresa.getEmpresa_id(), organizacao.getOrganizacao_id()
-					, usuarioOrgAcesso.getUsuario().getUsuario_id()) == null) {
+			if (this.usuarioOrgAcessoDao.buscaUsuarioOrgAcessoByEmpresaOrganizacaoUsuarioPerfil(empresa.getEmpresa_id(), organizacao.getOrganizacao_id(), usuarioOrgAcesso.getUsuario().getUsuario_id()) == null) {
 
 				usuarioOrgAcesso.setCreated(dataAtual);
 				usuarioOrgAcesso.setUpdated(dataAtual);
-	
+
 				usuarioOrgAcesso.setCreatedBy(usuarioOrgAcesso.getUsuario());
 				usuarioOrgAcesso.setUpdatedBy(usuarioOrgAcesso.getUsuario());
-	
-				usuarioOrgAcesso.setIsActive(usuarioOrgAcesso.getIsActive() == null ? false : true);
-	
+				usuarioOrgAcesso.setIsActive(usuarioOrgAcesso.getIsActive() == null ? false: true);
+
 				this.usuarioOrgAcessoDao.insert(usuarioOrgAcesso);
-	
+
 				mensagem = "Usuário adicionado com sucesso.";
 
 			} else {
 
 				mensagem = "Erro: Usuário já cadastrado.";
 
-			} 
+			}
 
 		} catch (Exception e) {
 
 			mensagem = "Erro: Falha ao adicionar o Usuário.";
 
-		} finally{
+		} finally {
 
 			this.usuarioOrgAcessoDao.clear();
 			this.usuarioOrgAcessoDao.close();
 
 		}
 
-		result.include("notice", mensagem);			
+		result.include("notice", mensagem);
+		result.redirectTo(this).cadastro();
+	}
+
+	@Post
+	@Path("/usuarioorgacesso/altera")
+	public void altera(Boolean isActive, Long empresa_id, Long organizacao_id, Long usuario_id) {
+
+		String mensagem = "";
+
+		UsuarioOrgAcesso usuarioOrgAcesso = null;
+
+		usuarioOrgAcesso = this.usuarioOrgAcessoDao.buscaUsuarioOrgAcessoByEmpresaOrganizacaoUsuarioPerfil(empresa_id, organizacao_id, usuario_id);
+
+		try {
+
+			usuarioOrgAcesso.setUpdated(dataAtual);
+			usuarioOrgAcesso.setUpdatedBy(usuario);
+			usuarioOrgAcesso.setIsActive(isActive);
+
+			this.usuarioOrgAcessoDao.altera(usuarioOrgAcesso);
+
+			mensagem = "Usuário alterado com sucesso.";
+
+		} catch (SQLException e) {
+
+			mensagem = "Erro: ao alterar o Usuário :";
+
+		} finally {
+
+			this.usuarioOrgAcessoDao.clear();
+			this.usuarioOrgAcessoDao.close();
+
+		}
+
+		result.include("notice", mensagem);
 		result.redirectTo(this).cadastro();
 
 	}
