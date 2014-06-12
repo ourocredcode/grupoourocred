5169fea3700b0c3b5193ae386a941ae214448a1e
ourocredcode <ourocredcode@gmail.com>
2014-06-12 15:40:14 +0000

    [Felipe Almeida]

--- sgo/src/br/com/sgo/controller/ContratoController.java
+++ sgo/src/br/com/sgo/controller/ContratoController.java
@@ -274,7 +274,7 @@ public class ContratoController {
 
 		for(ParceiroLocalidade pl : parceiroLocalidadeDao.buscaParceiroLocalidades(parceiroNegocio.getParceiroNegocio_id())){
 
-			if(pl.getTipoEndereco().getNome().equals("Assinatura")){
+			if(pl.getTipoEndereco().getNome().equals("Assinatura") && pl.getIsActive()){
 				parceiroLocalidade = pl;
 			}
 
